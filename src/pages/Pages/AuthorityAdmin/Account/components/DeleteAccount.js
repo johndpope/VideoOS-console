282159e8c7c7138d69feade0a81847bf4d8188de
Jian Zhang <zhangjian@videopls.com>
2018-09-12 19:16:55 +0800

    [fixed] change delete params

--- src/pages/Pages/AuthorityAdmin/Account/components/DeleteAccount.js
+++ src/pages/Pages/AuthorityAdmin/Account/components/DeleteAccount.js
@@ -1,24 +1,26 @@
-import React, { Fragment } from 'react';
-import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
-import { Button } from '@icedesign/base';
+import React, { Fragment } from "react";
+import { Modal, ModalBody, ModalFooter, ModalHeader } from "reactstrap";
+import { Button } from "@icedesign/base";
 
-const DeleteAccount = ({shouldOpen, toggle, deleteAccount, record}) => (
+const DeleteAccount = ({ shouldOpen, toggle, deleteAccount, record }) => (
   <Fragment>
-    <Modal
-      isOpen={shouldOpen}  
-      toggle={toggle}
-    >
+    <Modal isOpen={shouldOpen} toggle={toggle}>
       <ModalHeader toggle={toggle}>删除账号</ModalHeader>
       <ModalBody>
-        <p>
-          确定删除该账号
-        </p>  
+        <p>确定删除该账号</p>
       </ModalBody>
       <ModalFooter>
         <Button onClick={toggle}>取消</Button>
-        <Button type="primary" onClick={() => {deleteAccount(record)}}>确认</Button>
+        <Button
+          type="primary"
+          onClick={() => {
+            deleteAccount({ userId: record.userId });
+          }}
+        >
+          确认
+        </Button>
       </ModalFooter>
-    </Modal>  
+    </Modal>
   </Fragment>
 );
 
--- src/pages/Pages/AuthorityAdmin/Account/reducer.js
+++ src/pages/Pages/AuthorityAdmin/Account/reducer.js
@@ -30,95 +30,95 @@ import {
   QUERY_ALL_ACCOUNTTYPES_FAILURE,
   SHOW_DELETEACCOUNT_MODAL,
   HIDE_DELETEACCOUNT_MODAL,
-  SET_CURRENT_PAGE,
-} from './constants';
+  SET_CURRENT_PAGE
+} from "./constants";
 
 // The initial state of the account
 const initialState = {};
 
 function aaAccountReducer(state = initialState, action) {
-  switch(action.type) {
+  switch (action.type) {
     case GET_ACCOUNTS_REQUEST:
       return Object.assign({}, state, {
-        isLoading: true,
+        isLoading: true
       });
     case GET_ACCOUNTS_SUCCESS:
       return Object.assign({}, state, {
         isLoading: false,
         accountResult: action.payload.userInfoList || [],
-        total: action.payload.totalRecord,
+        total: action.payload.totalRecord
       });
     case GET_ACCOUNTS_FAIL:
       return Object.assign({}, state, {
-        isLoading: false,
+        isLoading: false
       });
     case ADD_ACCOUNT_REQUEST:
       return Object.assign({}, state, {
-        isLoading: true,
+        isLoading: true
       });
     case ADD_ACCOUNT_SUCCESS:
       return Object.assign({}, state, {
-        isLoading: false,
+        isLoading: false
       });
     case ADD_ACCOUNT_FAIL:
       return Object.assign({}, state, {
         addAccountResErr: action.payload.resMsg,
-        isLoading: false,
+        isLoading: false
       });
     case UPDATE_ACCOUNT_REQUEST:
       return Object.assign({}, state, {
-        isLoading: true,
+        isLoading: true
       });
     case UPDATE_ACCOUNT_SUCCESS:
       return Object.assign({}, state, {
-        isLoading: false,
+        isLoading: false
       });
     case UPDATE_ACCOUNT_FAILURE:
       return Object.assign({}, state, {
-        isLoading: false,
+        isLoading: false
       });
     case DELETE_ACCOUNT_REQUEST:
       return Object.assign({}, state, {
-        isLoading: true,
+        isLoading: true
       });
     case DELETE_ACCOUNT_SUCCESS:
       return Object.assign({}, state, {
-        isLoading: false,
+        isLoading: false
       });
     case DELETE_ACCOUNT_FAILURE:
       return Object.assign({}, state, {
-        isLoading: false,
+        isLoading: false
       });
     case QUERY_ALL_ACCOUNTTYPES_REQUEST:
       return Object.assign({}, state, {
-        isLoading: true,
+        isLoading: true
       });
     case QUERY_ALL_ACCOUNTTYPES_SUCCESS:
       return Object.assign({}, state, {
         roleTypes: action.payload,
-        isLoading: false,
+        isLoading: false
       });
     case QUERY_ALL_ACCOUNTTYPES_FAILURE:
       return Object.assign({}, state, {
-        isLoading: false,
+        isLoading: false
       });
     case SHOW_ADDACCOUNT_MODAL:
     case HIDE_ADDACCOUNT_MODAL:
       return Object.assign({}, state, {
         record: action.payload,
-        shouldAddAccountModalOpen: action.shouldOpen,
+        shouldAddAccountModalOpen: action.shouldOpen
       });
     case SHOW_DELETEACCOUNT_MODAL:
     case HIDE_DELETEACCOUNT_MODAL:
       return Object.assign({}, state, {
-        record: {roleId: action.payload && action.payload.roleId || ''},
-        shouldDeleteAccountModalOpen: action.shouldOpen,
+        record: { userId: (action.payload && action.payload.userId) || "" },
+        shouldDeleteAccountModalOpen: action.shouldOpen
       });
     case SET_CURRENT_PAGE:
-      return {...state, currentPage: action.payload.currentPage};
+      return { ...state, currentPage: action.payload.currentPage };
     default:
       return state;
-  }  
+  }
 }
 
 export default aaAccountReducer;
