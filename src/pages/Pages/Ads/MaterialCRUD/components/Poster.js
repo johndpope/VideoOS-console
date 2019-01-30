3dcd329b45ff3b4b92af8cb460acf0ebdd743a73
video++ <>
2019-01-30 18:16:48 +0800

    修改form验证

--- src/pages/Pages/Ads/MaterialCRUD/components/Poster.js
+++ src/pages/Pages/Ads/MaterialCRUD/components/Poster.js
@@ -3,7 +3,6 @@ import { Label, Input, Row, Col } from "reactstrap";
 import { Feedback } from "@icedesign/base";
 
 import { addMaterialFile } from "../api";
-
 export default class Poster extends Component {
   constructor(props) {
     super(props);
@@ -69,7 +68,6 @@ export default class Poster extends Component {
       creativeIdList = []
     } = this.state;
     const { creativeName, interactionTemplateId } = this.state;
-
     return (
       <Fragment>
         <div>基础配置</div>
@@ -263,11 +261,11 @@ export default class Poster extends Component {
             <Label>跳转外链链接</Label>
             <Input
               type="url"
-              readOnly={readonly}
               value={linkUrl}
               placeholder="请输入链接"
               onChange={e => {
                 linkUrl = e.target.value;
+                if (linkUrl === "") linkUrl = undefined;
                 this.setState({ linkUrl }, () =>
                   this.props.onChange(this.state)
                 );
